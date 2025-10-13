8502268a4d1303a278bfe21689e8e7b8115f08a3
Gaby <Gaby@Gaby>
2025-10-13 16:40:50 -0500

    Tercer comit ( diseño, añadimos las operaciones faltantes del crud "solo su funcionamiento")

--- gestion-nombre-cursos/src/main/java/com/unbosque/gestiocursos/gestion_nombre_cursos/controller/CursoController.java
+++ gestion-nombre-cursos/src/main/java/com/unbosque/gestiocursos/gestion_nombre_cursos/controller/CursoController.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.unbosque.gestiocursos.gestion_nombre_cursos.entity.Curso;
 import com.unbosque.gestiocursos.gestion_nombre_cursos.repository.CursoRepository;
+
+import ch.qos.logback.core.model.processor.PhaseIndicator;
 
 @Controller
 public class CursoController {
@@ -47,9 +50,46 @@ public class CursoController {
 			redirectAttributes.addFlashAttribute("message", "Curso guardado con exito");
 
 		} catch (Exception e) {
-			redirectAttributes.addFlashAttribute("mesage", e.getMessage());
+			redirectAttributes.addFlashAttribute("message", e.getMessage());
 		}
 
 		return "redirect:/cursos";
 	}
+	
+	@GetMapping("/cursos/{id}")
+	public String editarCurso(@PathVariable Integer id, Model model, RedirectAttributes redirectAttributes) {
+
+		try {
+
+			Curso curso = cursoRepository.findById(id).get();
+			model.addAttribute("pageTitle", "Actuaizar curso: "+id);
+			model.addAttribute("curso",curso);
+
+			redirectAttributes.addFlashAttribute("message", "Curso actualizado con exito");
+			
+			return "agregar_curso_form";
+
+		} catch (Exception e) {
+			redirectAttributes.addFlashAttribute("message", e.getMessage());
+		}
+
+		return "redirect:/cursos";
+	}
+	
+	@GetMapping("/cursos/delete/{id}")
+	public String eliminarCurso(@PathVariable Integer id,Model model, RedirectAttributes redirectAttributes) {
+		
+		try {
+			cursoRepository.deleteById(id);
+			redirectAttributes.addFlashAttribute("message","Curso Eliminado");
+			
+		} catch (Exception e) {
+			redirectAttributes.addFlashAttribute("message", e.getMessage());
+		}
+		return "redirect:/cursos";
+	}
+
+	
+	
+	
 }
