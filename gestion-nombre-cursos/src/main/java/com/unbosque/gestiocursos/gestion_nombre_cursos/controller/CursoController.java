4a6d41d76af7640fa4dd02bb9d5e6cc6bc2b5829
Juan David Guarnizo <guarnizoj032@gmail.com>
2025-11-03 18:16:32 -0500

    Septimo commit ( Paginacion y busqueda primera fase )

--- gestion-nombre-cursos/src/main/java/com/unbosque/gestiocursos/gestion_nombre_cursos/controller/CursoController.java
+++ gestion-nombre-cursos/src/main/java/com/unbosque/gestiocursos/gestion_nombre_cursos/controller/CursoController.java
@@ -3,11 +3,15 @@ package com.unbosque.gestiocursos.gestion_nombre_cursos.controller;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,11 +25,9 @@ import com.unbosque.gestiocursos.gestion_nombre_cursos.reports.CursoExporterExce
 import com.unbosque.gestiocursos.gestion_nombre_cursos.reports.CursoExporterPDF;
 import com.unbosque.gestiocursos.gestion_nombre_cursos.repository.CursoRepository;
 
-import ch.qos.logback.core.model.processor.PhaseIndicator;
 import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class CursoController {
@@ -39,7 +41,43 @@ public class CursoController {
 	}
 
 	@GetMapping("/cursos")
-	public String listarCursos(Model model) {
+	public String listarCursos(Model model, @Param("keyword") String keyword,@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "4") int size) {
+		
+		/*
+		 * Son Parametros para que la busqueda de cursos esten paginadas en un tamaño de  por pagina 
+		 *  @Param("keyword") String keyword,@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "4") int size
+		 */
+		
+		try {
+			List<Curso> cursos = new ArrayList<Curso>();
+			Pageable paging = PageRequest.of(page - 1, size);
+
+			Page<Curso> pageCursos = null;
+
+			
+			/*
+			 *Si en la busqueda no se escribe nada se paginaran todos los cursos que encuentre
+			 *En caso de que no sea asi hara la busqueda con la paginacion pero con la palabra clave (keyword) 
+			 */
+			if (keyword == null) {
+				pageCursos = cursoRepository.findAll(paging);
+			} else {
+
+				pageCursos = cursoRepository.findByTituloContainingIgnoreCase(keyword, paging);
+				model.addAttribute("keyword", keyword);
+			}
+			cursos = pageCursos.getContent();
+			model.addAttribute("cursos", cursos);
+			model.addAttribute("currentPage", pageCursos.getNumber());
+
+			model.addAttribute("totalItems", pageCursos.getTotalElements());
+			model.addAttribute("totalPages", pageCursos.getTotalPages());
+			model.addAttribute("pageSize", size);
+
+		} catch (Exception e) {
+			model.addAttribute("message", e.getMessage());
+		}
+
 		List<Curso> cursos = cursoRepository.findAll();
 		model.addAttribute("cursos", cursos);
 		return "cursos";
@@ -67,18 +105,18 @@ public class CursoController {
 
 		return "redirect:/cursos";
 	}
-	
+
 	@GetMapping("/cursos/{id}")
 	public String editarCurso(@PathVariable Integer id, Model model, RedirectAttributes redirectAttributes) {
 
 		try {
 
 			Curso curso = cursoRepository.findById(id).get();
-			model.addAttribute("pageTitle", "Actuaizar curso: "+id);
-			model.addAttribute("curso",curso);
+			model.addAttribute("pageTitle", "Actuaizar curso: " + id);
+			model.addAttribute("curso", curso);
 
 			redirectAttributes.addFlashAttribute("message", "Curso actualizado con exito");
-			
+
 			return "agregar_curso_form";
 
 		} catch (Exception e) {
@@ -87,14 +125,14 @@ public class CursoController {
 
 		return "redirect:/cursos";
 	}
-	
+
 	@GetMapping("/cursos/delete/{id}")
-	public String eliminarCurso(@PathVariable Integer id,Model model, RedirectAttributes redirectAttributes) {
-		
+	public String eliminarCurso(@PathVariable Integer id, Model model, RedirectAttributes redirectAttributes) {
+
 		try {
 			cursoRepository.deleteById(id);
-			redirectAttributes.addFlashAttribute("message","Curso Eliminado");
-			
+			redirectAttributes.addFlashAttribute("message", "Curso Eliminado");
+
 		} catch (Exception e) {
 			redirectAttributes.addFlashAttribute("message", e.getMessage());
 		}
@@ -103,35 +141,34 @@ public class CursoController {
 
 	@GetMapping("/export/pdf")
 	public void generarReportePDF(HttpServletResponse response) throws DocumentException, IOException {
-	response.setContentType("application/pdf");
-	DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
-	String currentDateTime = dateFormat.format(new Date());
-		
-	String headerKey="Content-Disposition";
-	String headerValue = "attachment; filename=cursos_"+currentDateTime+".pdf";
-	response.setHeader(headerKey, headerValue);
-	
-	List<Curso>cursos = cursoRepository.findAll();
-	
-	CursoExporterPDF exporterPDF = new CursoExporterPDF(cursos);
-	exporterPDF.export(response);
+		response.setContentType("application/pdf");
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+		String currentDateTime = dateFormat.format(new Date());
+
+		String headerKey = "Content-Disposition";
+		String headerValue = "attachment; filename=cursos_" + currentDateTime + ".pdf";
+		response.setHeader(headerKey, headerValue);
+
+		List<Curso> cursos = cursoRepository.findAll();
+
+		CursoExporterPDF exporterPDF = new CursoExporterPDF(cursos);
+		exporterPDF.export(response);
 	}
-	
+
 	@GetMapping("/export/excel")
 	public void generarReporteExcel(HttpServletResponse response) throws DocumentException, IOException {
 		response.setContentType("application/octet-stream");
 		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
 		String currentDateTime = dateFormat.format(new Date());
-		
+
 		String headerKey = "Content-Disposition";
 		String headerValue = "attachment; filename=cursos_" + currentDateTime + ".xlsx";
 		response.setHeader(headerKey, headerValue);
 
-		
-		List<Curso>cursos = cursoRepository.findAll();
-		
+		List<Curso> cursos = cursoRepository.findAll();
+
 		CursoExporterExcel exporterExcel = new CursoExporterExcel(cursos);
 		exporterExcel.export(response);
 	}
-	
+
 }
--- gestion-nombre-cursos/src/main/java/com/unbosque/gestiocursos/gestion_nombre_cursos/repository/CursoRepository.java
+++ gestion-nombre-cursos/src/main/java/com/unbosque/gestiocursos/gestion_nombre_cursos/repository/CursoRepository.java
@@ -1,5 +1,7 @@
 package com.unbosque.gestiocursos.gestion_nombre_cursos.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.unbosque.gestiocursos.gestion_nombre_cursos.entity.Curso;
 @Repository
 public interface CursoRepository extends JpaRepository<Curso,Integer>{
 
+	Page<Curso> findByTituloContainingIgnoreCase(String keyword,Pageable pageable);
 }
